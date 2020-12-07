3de4876faaefc1474c3616b75189779ecc8c6757
Gio <gv.mombelli@gmail.com>
2020-12-07 12:03:31 +0100

    add delete

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,12 @@ admin.initializeApp({
 // importo il DB
 const db = admin.firestore();
 
-function genId(users){
-    // Ternario -> ? - if ; : - else
+// function genId(users){
+//     // Ternario -> ? - if ; : - else
+//     return users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1;
+// }
+
+function genId(users) {
     return users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1;
 }
 
@@ -46,26 +50,52 @@ router.post("/users", async (req, res) => {
     // aspetto il DB per evitare che si creino elementi con il medesimo id
     const list = await db.collection("users").get();
     list.forEach(doc => users.push(doc.data()));
-
     const newId = genId(users);
     let user = {
         id: newId,
         name: req.body.name
     };
     users.push(user);
-    db.collection('users').doc(newId.toString()).set(user);
+    db.collection("users").doc(newId.toString()).set(user);
     return res.status(201).json({ message: "Created"});
 });
 
 //UPDATE (patch)
-router.patch("/users/:id", (req,res) => {
+router.patch("/users/:id", async (req,res) => {
+    // Aggiorno la lista
+    users.length = 0;
+    const list = await db.collection("users").get();
+    list.forEach(doc => users.push(doc.data()));
+
+    if (!req.body.name){
+        return res.status(400).json({message: "You have to pass a name :/"});
+    }
+    const u = await db.collection("users").doc(req.params.id).get();
+    if (!u.data()){
+        return res.status(404).json({message: "User not found :("});
+    }
+
+    // In remoto
+    db.collection("users").doc(req.params.id).set({name: req.body.name}, {merge: true});
+    // In locale
     const user = users.find(valore => valore.id === Number(req.params.id));
     user.name = req.body.name;
     return res.json({message: "Updated"});
 });
 
 // DELETE
-router.delete("/users/:id", (req,res) => {
+router.delete("/users/:id", async (req,res) => {
+    // Aggiorno la lista
+    users.length = 0;
+    const list = await db.collection("users").get();
+    list.forEach(doc => users.push(doc.data()));
+
+    const u = await db.collection("users").doc(req.params.id).get();
+    if (!u.data()){
+        return res.status(404).json({message: "User not found :("});
+    }
+
+    db.collection("users").doc(req.params.id).delete()
     const userIndex = users.findIndex(valore => valore.id === Number(req.params.id));
     users.splice(userIndex, 1);
     return res.json({message: "Deleted"});
